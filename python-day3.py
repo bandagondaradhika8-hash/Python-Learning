1fd5f71acfe140b46e53169fcdb301dfdfcecec8
bandagondaradhika8-hash <bandagonda.radhika8@gmail.com>
2026-07-08 21:16:41 +0530

    Add files via upload

---
+++ python-day3.py
@@ -0,0 +1,23 @@
+for i in range(5):
+    print(i)
+for i in range(5):
+    print("hello")
+############## i stores the current values from range #########
+for i in range(3):
+    print(i * 2)# 0*2 is 0 ...1*2 is 2....2*2is 4soooooooooo024
+
+######to print numbers from 1 to 10#######
+for i in range(11):
+    print(i)
+    ##########
+for i in range (2,20,+2):
+    print(i)
+    ########## start in 5rows 5 columns
+for i in range(5):
+    print("*****")
+    ########## to print python 5 times
+for i in range(5):
+    print("python")
+    ########## to print only odd nms from 1 to 19
+for i in range(1,20,+2):
+    print(i)
